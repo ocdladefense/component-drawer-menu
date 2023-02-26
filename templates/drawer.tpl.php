5eecc5bea680b4543cdc21399eac9a770f8ed660
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-02-26 18:23:41 +0000

    Remove XML link from drawer.

--- templates/drawer.tpl.php
+++ templates/drawer.tpl.php
@@ -14,9 +14,13 @@
 
     <ul id="sidebar-links" class="sidebar-links">
 						
-        <li id="home" class="side-menu-item"><a href="/home"><i class="fas fa-home" aria-hidden="true"></i>home</a></li>
+        <li id="home" class="side-menu-item">
+            <a href="/home"><i class="fas fa-home" aria-hidden="true"></i>home</a>
+        </li>
 
-        <li id="search" class="side-menu-item"><a href="/example/search/duii"><i class="fas fa-magnifying-glass" aria-hidden="true"></i>search</a></li>
+        <li id="search" class="side-menu-item">
+            <a href="/example/search/duii"><i class="fas fa-magnifying-glass" aria-hidden="true"></i>search</a>
+        </li>
 
 
         <li id="ocdla-org" class="side-menu-item">
@@ -36,7 +40,7 @@
                 <i class="fas fa-solid fa-book-bookmark" aria-hidden="true"></i>legal resource library</a>
         </li>
 
-        <li id="xml" class="side-menu-item"><a href="/repository/product2"><i class="fas fa-home" aria-hidden="true"></i>xml</a></li>
+        <!-- <li id="xml" class="side-menu-item"><a href="/repository/product2"><i class="fas fa-home" aria-hidden="true"></i>xml</a></li> -->
        
         <!--<li class="side-menu-item"><i class="fas fa-people-arrows fa-2x"></i><a href="/covid" title="How OCDLA is pivoting to meet COVID-19 challenges.">covid-19</a></li>-->
 
@@ -46,21 +50,31 @@
 
         <!--<li class="side-menu-item"><i class="fas fa-calendar-day fa-2x" aria-hidden="true"></i><a href="/events">events</a></li>-->
 
-        <li id="jobs" class="side-menu-item"><a href="/jobs"><i class="fas fa-briefcase" aria-hidden="true"></i>jobs</a></li>
+        <li id="jobs" class="side-menu-item">
+            <a href="/jobs"><i class="fas fa-briefcase" aria-hidden="true"></i>jobs</a>
+        </li>
 
-        <li id="car" class="side-menu-item"><a href="/car/list"><i class="fas fa-database" aria-hidden="true"></i>case reviews</a></li>
+        <li id="car" class="side-menu-item">
+            <a href="/car/list"><i class="fas fa-database" aria-hidden="true"></i>case reviews</a>
+        </li>
 
         <!--<li class="side-menu-item"><i class="fas fa-map-pin fa-2x" aria-hidden="true"></i><a href="/maps">maps</a></li>-->
 
         <!--<li class="side-menu-item"><i class="fas fa-video fa-2x" aria-hidden="true"></i><a href="/videos">videos</a></li>-->
 
-        <li id="member" class="side-menu-item"><a href="/directory/members"><i class="fas fa-user-friends" aria-hidden="true"></i>member directory</a></li>
+        <li id="member" class="side-menu-item">
+            <a href="/directory/members"><i class="fas fa-user-friends" aria-hidden="true"></i>member directory</a>
+        </li>
         
-        <li id="expert" class="side-menu-item"><a href="/directory/experts"><i class="fas fa-user-friends" aria-hidden="true"></i>expert directory</a></li>
+        <li id="expert" class="side-menu-item">
+            <a href="/directory/experts"><i class="fas fa-user-friends" aria-hidden="true"></i>expert directory</a>
+        </li>
 
         <!--<li class="side-menu-item"><i class="fas fa-comment-dots fa-2x" aria-hidden="true"></i><a href="/feedback">feedback</a></li>-->
 
-        <li class="side-menu-item"><a href="https://www.ocdla.org/contact-us" target="_new"><i class="fas fa-mobile-alt" aria-hidden="true"></i>contact us</a></li>
+        <li class="side-menu-item">
+            <a href="<?php print ORG_URL . "/contact-us"; ?>" target="_new"><i class="fas fa-mobile-alt" aria-hidden="true"></i>contact us</a>
+        </li>
         
 
     </ul>
